65c13960ddd43f5fec74ffdc28886632b1f9ac24
mac <mac@10.0.0.20>
2020-04-20 14:55:48 +0800

    引入posscss

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="index">
-    <!-- <test /> -->
+    <div class="title">普通创业篇 - 推箭心选</div>
+    <div class="head"></div>
   </div>
 </template>
 
@@ -30,5 +31,21 @@ export default class Index extends VueBase {
 </script>
 
 <style scoped>
+.title {
+  font-size:42px;
+  color:rgba(60,65,70,1);
+}
+.head {
+  /* width:29rem;
+  height:6.67rem;
+  background:rgba(247,248,249,1);
+  border-radius:0.42rem;
+  margin-left: 1.08rem; */
+  width:696px;
+  height:160px;
+  background:rgba(247,248,249,1);
+  border-radius:10px;
+  margin-left: 26px;
+}
 </style>
 
